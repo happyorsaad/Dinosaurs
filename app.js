5feebb40b7f9150e3f63ee60a7e7d5798a412633
Saad Hussain <saadhussain@Saads-MacBook-Pro.local>
2021-06-18 18:44:29 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// Create Dino Constructor
-
-// Create Dino Objects
-
-// Create Human Object
-
-// Use IIFE to get human data from form
-
-// Create Dino Compare Method 1
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-// Create Dino Compare Method 2
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-// Create Dino Compare Method 3
-// NOTE: Weight in JSON file is in lbs, height in inches.
-
-// Generate Tiles for each Dino in Array
-
-// Add tiles to DOM
-
-// Remove form from screen
-
-// On button click, prepare and display infographic
-
 function Dinosaur(data) {
   this.species = data.species;
   this.diet = data.diet;
